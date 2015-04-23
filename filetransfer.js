0555edca30fef8e91f4bbb627034bc90159e0290
markr79 <mrubenchik@gmail.com>
2015-04-23 11:44:21 -0400

    Support node.js

--- filetransfer.js
+++ filetransfer.js
@@ -1,12 +1,20 @@
+var async = require('async');
+//var webrtcsupport = require('webrtcsupport');
 var WildEmitter = require('wildemitter');
 var util = require('util');
+var hashes = require('iana-hashes');
+var window = window || {}
+var FileReader = window.FileReader || require('filereader');
+var Blob = window.FileReader || require('blob');
 
 function Sender(opts) {
     WildEmitter.call(this);
+    var self = this;
     var options = opts || {};
     this.config = {
         chunksize: 16384,
-        pacing: 0
+        pacing: 10,
+        hash: 'sha-1' // note: this uses iana hash names
     };
     // set our config from options
     var item;
@@ -16,40 +24,75 @@ function Sender(opts) {
 
     this.file = null;
     this.channel = null;
+    this.hash = null;
+
+    // paced sender
+    // TODO: do we have to do this?
+    this.processingQueue = async.queue(function (task, next) {
+        if (task.type == 'chunk') {
+            var reader = new FileReader();
+            reader.onload = (function() {
+                return function(e) {
+                    self.channel.send(e.target.result);
+
+                    if (self.hash) {
+                        self.hash.update(new Uint8Array(e.target.result));
+                    }
+
+                    self.emit('progress', task.start, task.file.size);
+
+                    setTimeout(next, self.config.pacing); // pacing
+                };
+            })(task.file);
+            var slice = task.file.slice(task.start, task.start + task.size);
+            reader.readAsArrayBuffer(slice);
+        } else if (task.type == 'complete') {
+            self.emit('progress', self.file.size, self.file.size);
+            self.emit('sentFile', self.hash ? {hash: self.hash.digest('hex'), algo: self.config.hash } : null);
+            next();
+        }
+    });
 }
 util.inherits(Sender, WildEmitter);
 
 Sender.prototype.send = function (file, channel) {
-    var self = this;
     this.file = file;
+    if (this.config.hash) {
+        this.hash = hashes.createHash(this.config.hash);
+    }
+
     this.channel = channel;
-    var sliceFile = function(offset) {
-        var reader = new window.FileReader();
-        reader.onload = (function() {
-            return function(e) {
-                self.channel.send(e.target.result);
-                self.emit('progress', offset, file.size, e.target.result);
-                if (file.size > offset + e.target.result.byteLength) {
-                    window.setTimeout(sliceFile, self.config.pacing, offset + self.config.chunksize);
-                } else {
-                    self.emit('progress', file.size, file.size, null);
-                    self.emit('sentFile');
-                }
-            };
-        })(file);
-        var slice = file.slice(offset, offset + self.config.chunksize);
-        reader.readAsArrayBuffer(slice);
-    };
-    window.setTimeout(sliceFile, 0, 0);
+    // FIXME: hook to channel.onopen?
+    for (var start = 0; start < this.file.size; start += this.config.chunksize) {
+        this.processingQueue.push({
+            type: 'chunk',
+            file: file,
+            start: start,
+            size: this.config.chunksize
+        });
+    }
+    this.processingQueue.push({
+        type: 'complete'
+    });
 };
 
-function Receiver() {
+function Receiver(opts) {
     WildEmitter.call(this);
 
+    var options = opts || {};
+    this.config = {
+        hash: 'sha-1'
+    };
+    // set our config from options
+    var item;
+    for (item in options) {
+        this.config[item] = options[item];
+    }
     this.receiveBuffer = [];
     this.received = 0;
     this.metadata = {};
     this.channel = null;
+    this.hash = null;
 }
 util.inherits(Receiver, WildEmitter);
 
@@ -59,6 +102,10 @@ Receiver.prototype.receive = function (metadata, channel) {
     if (metadata) {
         this.metadata = metadata;
     }
+    if (this.config.hash) {
+        this.hash = hashes.createHash(this.config.hash);
+    }
+
     this.channel = channel;
     // chrome only supports arraybuffers and those make it easier to calc the hash
     channel.binaryType = 'arraybuffer';
@@ -67,9 +114,16 @@ Receiver.prototype.receive = function (metadata, channel) {
         self.received += len;
         self.receiveBuffer.push(event.data);
 
-        self.emit('progress', self.received, self.metadata.size, event.data);
-        if (self.received === self.metadata.size) {
-            self.emit('receivedFile', new window.Blob(self.receiveBuffer), self.metadata);
+        if (self.hash) {
+            self.hash.update(new Uint8Array(event.data));
+        }
+
+        self.emit('progress', self.received, self.metadata.size);
+        if (self.received == self.metadata.size) {
+            if (self.hash) {
+                self.metadata.actualhash = self.hash.digest('hex');
+            }
+            self.emit('receivedFile', new Blob(self.receiveBuffer), self.metadata);
             self.receiveBuffer = []; // discard receivebuffer
         } else if (self.received > self.metadata.size) {
             // FIXME
@@ -81,6 +135,6 @@ Receiver.prototype.receive = function (metadata, channel) {
 };
 
 module.exports = {};
-module.exports.support = window && window.File && window.FileReader && window.Blob;
+module.exports.support = FileReader && Blob;
 module.exports.Sender = Sender;
 module.exports.Receiver = Receiver;
